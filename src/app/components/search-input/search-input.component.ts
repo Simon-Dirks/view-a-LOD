e505930705750c90685366838a05026acd6e381c
Simon <Simon-Dirks@users.noreply.github.com>
2024-04-03 16:07:17 +0200

    Search on page load

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../../services/search.service';
 import { FormsModule } from '@angular/forms';
 
@@ -9,10 +9,14 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.scss',
 })
-export class SearchInputComponent {
-  searchInput: string = '';
+export class SearchInputComponent implements OnInit {
+  searchInput: string = 'Margaretha';
 
   constructor(public search: SearchService) {}
+
+  ngOnInit() {
+    this.onSearchInputChange(this.searchInput);
+  }
 
   onSearchInputChange(input: string) {
     void this.search.execute(input);
