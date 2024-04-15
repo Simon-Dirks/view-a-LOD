9014872511a99ab09745c24cea348cd310ccfa67
Simon <Simon-Dirks@users.noreply.github.com>
2024-04-15 11:20:27 +0200

    Retry requests if they fail (closes #28)

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -17,22 +17,40 @@ export class ApiService {
     }
   }
 
-  async postData<T>(url: string, data: any): Promise<T> {
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-      if (!response.ok) {
-        return Promise.reject(new Error('Network response was not ok'));
+  async postData<T>(
+    url: string,
+    data: any,
+    maxRetries: number = 3,
+    retryInterval: number = 2000,
+  ): Promise<T> {
+    let retries = 0;
+    while (retries < maxRetries) {
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return (await response.json()) as T;
+      } catch (error) {
+        console.error('There was a problem with the API request:', error);
+        retries++;
+        if (retries < maxRetries) {
+          console.log(`Retrying in ${retryInterval / 1000} seconds...`);
+          await new Promise((resolve) => setTimeout(resolve, retryInterval));
+        } else {
+          throw error;
+        }
       }
-      return (await response.json()) as T;
-    } catch (error) {
-      console.error('There was a problem with the API request:', error);
-      throw error;
     }
+
+    throw new Error(
+      `There was a problem with the API request: ${url} ${JSON.stringify(data)}`,
+    );
   }
 }
