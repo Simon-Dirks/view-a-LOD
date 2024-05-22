7bc408fb5c5fe75ddb3a9132f16aa6a7918a929c
Simon <Simon-Dirks@users.noreply.github.com>
2024-05-22 12:32:16 +0200

    Update photograph render component (closes #102)

--- src/app/components/node/node-render-components/type-render-components/sdo-photograph/sdo-photograph.component.ts
+++ src/app/components/node/node-render-components/type-render-components/sdo-photograph/sdo-photograph.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NodeRenderComponent } from '../../node-render.component';
-import { JsonPipe, NgForOf } from '@angular/common';
+import { JsonPipe, NgForOf, NgIf } from '@angular/common';
 import { Settings } from '../../../../../config/settings';
 import { NodeLinkComponent } from '../../../node-link/node-link.component';
 import { NodeTableViewComponent } from '../../node-table-view/node-table-view.component';
@@ -8,7 +8,7 @@ import { NodeTableViewComponent } from '../../node-table-view/node-table-view.co
 @Component({
   selector: 'app-sdo-photograph',
   standalone: true,
-  imports: [JsonPipe, NgForOf, NodeLinkComponent, NodeTableViewComponent],
+  imports: [JsonPipe, NgForOf, NodeLinkComponent, NodeTableViewComponent, NgIf],
   templateUrl: './sdo-photograph.component.html',
   styleUrl: './sdo-photograph.component.scss',
 })
@@ -17,12 +17,21 @@ export class SdoPhotographComponent
   implements OnInit
 {
   commentIds: string[] = [];
+  dates: string[] = [];
+  creators: string[] = [];
   imageUrls: string[] = [];
 
   ngOnInit(): void {
     this.commentIds = this.nodes.getObjValues(this.node, [
       'https://schema.org/comment',
     ]);
+    this.dates = this.nodes.getObjValues(this.node, [
+      'https://www.ica.org/standards/RiC/ontology#expressedDateValue',
+    ]);
+    this.creators = this.nodes.getObjValues(this.node, [
+      'https://www.ica.org/standards/RiC/ontology#hasCreator',
+    ]);
+
     this.imageUrls = this.nodes.getObjValues(
       this.node,
       Settings.predicates.images,
@@ -30,4 +39,5 @@ export class SdoPhotographComponent
   }
 
   protected readonly Settings = Settings;
+  protected readonly Object = Object;
 }
