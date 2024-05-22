5f0d21d09a7601c2c544e0b3e243686b01fc23ae
Simon <Simon-Dirks@users.noreply.github.com>
2024-05-22 12:49:37 +0200

    Remove unused details component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './components/views/search/search.component';
-import { DetailsComponent } from './components/views/details/details.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: SearchComponent,
     pathMatch: 'full',
-  },
-  {
-    path: 'details/:id',
-    component: DetailsComponent,
   },
   {
     path: '**',
