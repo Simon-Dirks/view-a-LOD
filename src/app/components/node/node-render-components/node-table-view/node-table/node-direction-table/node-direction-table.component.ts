ac48e27c47714605706ee92a39737145c181c774
Simon <Simon-Dirks@users.noreply.github.com>
2024-05-23 11:50:15 +0200

    Create (visually) separate section for incoming relations

---
+++ src/app/components/node/node-render-components/node-table-view/node-table/node-direction-table/node-direction-table.component.ts
@@ -0,0 +1,101 @@
+import { Component, Input } from '@angular/core';
+import { NgClass, NgForOf, NgIf } from '@angular/common';
+import { Direction, NodeModel } from '../../../../../../models/node.model';
+import { PredicateVisibility } from '../../../../../../models/settings/predicate-visibility-settings.model';
+import { NodeService } from '../../../../../../services/node.service';
+import { SettingsService } from '../../../../../../services/settings.service';
+import { sortByArrayOrder } from '../../../../../../helpers/util.helper';
+import {
+  NodeTableCellComponent,
+  TableCellShowOptions,
+} from '../node-table-cell/node-table-cell.component';
+import { NodeLinkComponent } from '../../../../node-link/node-link.component';
+
+@Component({
+  selector: 'app-node-direction-table',
+  standalone: true,
+  imports: [NgForOf, NgClass, NodeTableCellComponent, NgIf, NodeLinkComponent],
+  templateUrl: './node-direction-table.component.html',
+  styleUrl: './node-direction-table.component.scss',
+})
+export class NodeDirectionTableComponent {
+  @Input() node?: NodeModel;
+  @Input() visibility!: PredicateVisibility;
+  @Input() direction!: Direction;
+
+  nodePreds: string[] = [];
+  predicateVisibilities: { [pred: string]: PredicateVisibility } = {};
+  numPredValues: {
+    [pred: string]: { [direction in Direction]: number };
+  } = {};
+
+  constructor(
+    public nodes: NodeService,
+    public settings: SettingsService,
+  ) {}
+
+  ngOnInit() {
+    this.initPredData();
+    this.initNodePreds();
+  }
+
+  initNodePreds() {
+    if (!this.node) {
+      return;
+    }
+    const nodePreds = Object.keys(this.node);
+    const sortedNodePreds = sortByArrayOrder(
+      nodePreds,
+      this.settings.getVisiblePredicates()[this.visibility],
+    );
+    this.nodePreds = sortedNodePreds;
+  }
+
+  initPredData() {
+    if (!this.node) {
+      return;
+    }
+    for (const pred of Object.keys(this.node)) {
+      this.predicateVisibilities[pred] =
+        this.settings.getPredicateVisibility(pred);
+
+      for (const direction of [Direction.Outgoing, Direction.Incoming]) {
+        if (!this.numPredValues[pred]) {
+          (this.numPredValues[pred] as any) = {};
+        }
+        this.numPredValues[pred][direction] =
+          this.nodes.getObjValuesByDirection(
+            this.node,
+            [pred],
+            direction,
+          ).length;
+      }
+    }
+  }
+
+  getNumPredValues(pred: string, direction: Direction) {
+    if (!pred || direction === undefined) {
+      return 0;
+    }
+
+    return this.numPredValues[pred][direction];
+  }
+
+  hasPredsToShow(direction: Direction, visibility: PredicateVisibility) {
+    return Object.entries(this.numPredValues).some(
+      ([pred, numValuesByDirection]) => {
+        return (
+          this.predicateVisibilities[pred] === visibility &&
+          numValuesByDirection[direction] > 0
+        );
+      },
+    );
+  }
+
+  get smallFontSize(): boolean {
+    return this.visibility === PredicateVisibility.Details;
+  }
+
+  protected readonly Direction = Direction;
+  protected readonly TableCellShowOptions = TableCellShowOptions;
+}
--- src/app/components/node/node-render-components/node-table-view/node-table/node-table.component.ts
+++ src/app/components/node/node-render-components/node-table-view/node-table/node-table.component.ts
@@ -3,14 +3,10 @@ import { Direction, NodeModel } from '../../../../../models/node.model';
 import { JsonPipe, NgClass, NgForOf, NgIf } from '@angular/common';
 import { NgIcon } from '@ng-icons/core';
 import { NodeLinkComponent } from '../../../node-link/node-link.component';
-import { NodeService } from '../../../../../services/node.service';
-import {
-  NodeTableCellComponent,
-  TableCellShowOptions,
-} from './node-table-cell/node-table-cell.component';
-import { SettingsService } from '../../../../../services/settings.service';
+import { NodeTableCellComponent } from './node-table-cell/node-table-cell.component';
 import { PredicateVisibility } from '../../../../../models/settings/predicate-visibility-settings.model';
-import { sortByArrayOrder } from '../../../../../helpers/util.helper';
+import { NodeDirectionTableComponent } from './node-direction-table/node-direction-table.component';
+import { NodeService } from '../../../../../services/node.service';
 
 @Component({
   selector: 'app-node-table',
@@ -23,74 +19,18 @@ import { sortByArrayOrder } from '../../../../../helpers/util.helper';
     NgClass,
     NodeTableCellComponent,
     JsonPipe,
+    NodeDirectionTableComponent,
   ],
   templateUrl: './node-table.component.html',
   styleUrl: './node-table.component.scss',
 })
 export class NodeTableComponent implements OnInit {
   @Input() node?: NodeModel;
-  @Input() smallFontSize = false;
   @Input() visibility!: PredicateVisibility;
 
-  nodePreds: string[] = [];
-  predicateVisibilities: { [pred: string]: PredicateVisibility } = {};
-  numPredValues: {
-    [pred: string]: { [direction in Direction]: number };
-  } = {};
+  constructor(public nodes: NodeService) {}
 
-  constructor(
-    public nodes: NodeService,
-    public settings: SettingsService,
-  ) {}
-
-  ngOnInit() {
-    this.initPredData();
-    this.initNodePreds();
-  }
-
-  initNodePreds() {
-    if (!this.node) {
-      return;
-    }
-    const nodePreds = Object.keys(this.node);
-    const sortedNodePreds = sortByArrayOrder(
-      nodePreds,
-      this.settings.getVisiblePredicates()[this.visibility],
-    );
-    this.nodePreds = sortedNodePreds;
-  }
-
-  initPredData() {
-    if (!this.node) {
-      return;
-    }
-    for (const pred of Object.keys(this.node)) {
-      this.predicateVisibilities[pred] =
-        this.settings.getPredicateVisibility(pred);
-
-      for (const direction of [Direction.Outgoing, Direction.Incoming]) {
-        if (!this.numPredValues[pred]) {
-          (this.numPredValues[pred] as any) = {};
-        }
-        this.numPredValues[pred][direction] =
-          this.nodes.getObjValuesByDirection(
-            this.node,
-            [pred],
-            direction,
-          ).length;
-      }
-    }
-  }
-
-  getNumPredValues(pred: string, direction: Direction) {
-    if (!pred || direction === undefined) {
-      return 0;
-    }
-
-    return this.numPredValues[pred][direction];
-  }
+  ngOnInit(): void {}
 
   protected readonly Direction = Direction;
-  protected readonly Object = Object;
-  protected readonly TableCellShowOptions = TableCellShowOptions;
 }
