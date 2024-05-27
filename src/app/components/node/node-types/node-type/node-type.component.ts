767d2ea9b905f04227064779732d006c592daca7
Simon <Simon-Dirks@users.noreply.github.com>
2024-05-27 14:46:53 +0200

    Show names of filter type clusters if available (closes #117)

--- src/app/components/node/node-types/node-type/node-type.component.ts
+++ src/app/components/node/node-types/node-type/node-type.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { NodeLinkComponent } from '../../node-link/node-link.component';
 import { TypeModel } from '../../../../models/type.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-node-type',
   standalone: true,
-  imports: [NodeLinkComponent],
+  imports: [NodeLinkComponent, NgClass],
   templateUrl: './node-type.component.html',
   styleUrl: './node-type.component.scss',
 })
 export class NodeTypeComponent {
   @Input() type?: TypeModel;
+  @Input() showNeutralColor = false;
 }
--- src/app/components/node/node-types/node-types.component.ts
+++ src/app/components/node/node-types/node-types.component.ts
@@ -5,6 +5,7 @@ import { NodeTypeComponent } from './node-type/node-type.component';
 import { ClusterService } from '../../../services/cluster.service';
 import { Settings } from '../../../config/settings';
 import { TypeModel } from '../../../models/type.model';
+import { intersects } from '../../../helpers/util.helper';
 
 @Component({
   selector: 'app-node-types',
@@ -15,18 +16,41 @@ import { TypeModel } from '../../../models/type.model';
 })
 export class NodeTypesComponent {
   @Input() types?: TypeModel[];
+  typesClusteredInFilters: TypeModel[] = [];
   clusteredTypes: TypeModel[] = [];
 
   constructor(public clusters: ClusterService) {}
 
   ngOnInit() {
+    this.initTypesClusteredInFilters();
     this.initClusteredTypes();
+  }
+
+  initTypesClusteredInFilters() {
+    if (!this.types) {
+      return;
+    }
+
+    this.typesClusteredInFilters = [];
+    const typeIds = this.types.map((t) => t.id);
+    for (const [clusterId, filterCluster] of Object.entries(
+      Settings.clustering.filterOptionValues,
+    )) {
+      const typesAreClustered = intersects(typeIds, filterCluster.valueIds);
+      if (typesAreClustered) {
+        this.typesClusteredInFilters.push({
+          id: clusterId,
+          label: filterCluster.label,
+        });
+      }
+    }
   }
 
   initClusteredTypes() {
     if (!this.types) {
       return;
     }
+
     this.clusteredTypes = this.clusters.clusterTypes(
       this.types,
       Settings.clustering.types,
