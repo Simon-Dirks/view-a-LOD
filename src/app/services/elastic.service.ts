85c9e2e6983ea126874bf236c849e3579a716cc5
Simon <Simon-Dirks@users.noreply.github.com>
2024-06-04 15:00:26 +0200

    Hide "curated" search results when selecting endpoint

--- src/app/services/elastic.service.ts
+++ src/app/services/elastic.service.ts
@@ -211,7 +211,9 @@ export class ElasticService {
 
     let searchFilters: FilterModel[] = filters;
     const isHomePageQuery = !query;
-    const hasNoFiltersYet = !filters || filters.length === 0;
+    const hasNoFiltersYet =
+      !filters ||
+      (filters.length === 0 && this.endpoints.enabledIds.value.length === 0);
     if (isHomePageQuery && hasNoFiltersYet) {
       searchFilters = Settings.filtersForEmptySearch as FilterModel[];
     }
