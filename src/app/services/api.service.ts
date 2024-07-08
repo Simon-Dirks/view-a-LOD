3bf498680058ecb1fb330860c498cb2a5cde70a3
Simon <Simon-Dirks@users.noreply.github.com>
2024-07-08 12:17:27 +0200

    Rewrite api post using httpclient

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  catchError,
+  delay,
+  lastValueFrom,
+  retryWhen,
+  scan,
+  throwError,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
+
   async fetchData<T>(url: string): Promise<T> {
     try {
       const response = await fetch(url);
@@ -23,34 +34,25 @@ export class ApiService {
     maxRetries: number = 3,
     retryInterval: number = 2000,
   ): Promise<T> {
-    let retries = 0;
-    while (retries < maxRetries) {
-      try {
-        const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        });
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return (await response.json()) as T;
-      } catch (error) {
-        console.error('There was a problem with the API request:', error);
-        retries++;
-        if (retries < maxRetries) {
-          console.log(`Retrying in ${retryInterval / 1000} seconds...`);
-          await new Promise((resolve) => setTimeout(resolve, retryInterval));
-        } else {
-          throw error;
-        }
-      }
-    }
-
-    throw new Error(
-      `There was a problem with the API request: ${url} ${JSON.stringify(data)}`,
+    return lastValueFrom(
+      this.http.post<T>(url, data).pipe(
+        catchError((error) => {
+          console.error('There was a problem with the API request:', error);
+          return throwError(error);
+        }),
+        retryWhen((errors) =>
+          errors.pipe(
+            scan((retryCount, error) => {
+              if (retryCount >= maxRetries) {
+                throw error;
+              }
+              console.log(`Retrying in ${retryInterval / 1000} seconds...`);
+              return retryCount + 1;
+            }, 0),
+            delay(retryInterval),
+          ),
+        ),
+      ),
     );
   }
 }
