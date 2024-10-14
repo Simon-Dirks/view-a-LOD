97add471d317393eba4f64573c244f80b6593acb
Simon <Simon-Dirks@users.noreply.github.com>
2024-10-14 16:10:53 +0200

    Add basic OSD placeholder

--- src/app/components/node/node-images/node-images.component.ts
+++ src/app/components/node/node-images/node-images.component.ts
@@ -1,14 +1,20 @@
 import {
+  AfterViewInit,
   Component,
+  ElementRef,
   Input,
+  NgZone,
   OnChanges,
+  OnDestroy,
   OnInit,
   SimpleChanges,
+  ViewChild,
 } from '@angular/core';
 import { JsonPipe, NgForOf, NgIf } from '@angular/common';
 import { Config } from '../../../config/config';
 import { UrlService } from '../../../services/url.service';
 import { Settings } from '../../../config/settings';
+import OpenSeadragon, { Viewer } from 'openseadragon';
 
 @Component({
   selector: 'app-node-images',
@@ -17,21 +23,53 @@ import { Settings } from '../../../config/settings';
   templateUrl: './node-images.component.html',
   styleUrl: './node-images.component.scss',
 })
-export class NodeImagesComponent implements OnInit, OnChanges {
+export class NodeImagesComponent
+  implements OnInit, OnChanges, OnDestroy, AfterViewInit
+{
+  private imageViewer?: Viewer;
+
+  @ViewChild('viewerElem') viewerElem!: ElementRef;
+
   @Input() imageUrls?: string[];
   @Input() width = '5rem';
 
   processedImageUrls: string[] = [];
 
-  constructor(public urlService: UrlService) {}
+  constructor(
+    public urlService: UrlService,
+    private ngZone: NgZone,
+  ) {}
 
   ngOnInit() {
     this.processImageUrls();
   }
 
+  ngAfterViewInit() {
+    this.initImageViewer();
+  }
+
+  initImageViewer() {
+    this.imageViewer = this.ngZone.runOutsideAngular(() =>
+      OpenSeadragon({
+        element: this.viewerElem.nativeElement,
+        prefixUrl: 'assets/osd/images/',
+        tileSources: {
+          type: 'image',
+          url: 'https://openseadragon.github.io/example-images/grand-canyon-landscape-overlooking.jpg',
+        },
+      }),
+    );
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['imageUrls']) {
       this.processImageUrls();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.imageViewer) {
+      this.imageViewer.destroy();
     }
   }
 
