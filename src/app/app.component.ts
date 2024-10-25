ad33f081cb431737fa5472601ec8376ebf243149
Simon <Simon-Dirks@users.noreply.github.com>
2024-10-25 12:02:02 +0200

    Revert "Show (empty) modal on image click"
    This reverts commit 034ed9483b428c323461702226f645e4aa3d2aec.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgIconComponent } from '@ng-icons/core';
-import { ImageModalComponent } from './components/image-modal/image-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgIconComponent, ImageModalComponent],
+  imports: [CommonModule, RouterOutlet, NgIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
