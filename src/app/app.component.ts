034ed9483b428c323461702226f645e4aa3d2aec
Simon <Simon-Dirks@users.noreply.github.com>
2024-10-25 12:01:37 +0200

    Show (empty) modal on image click
    TODO: Add OSD viewer to modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgIconComponent } from '@ng-icons/core';
+import { ImageModalComponent } from './components/image-modal/image-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgIconComponent],
+  imports: [CommonModule, RouterOutlet, NgIconComponent, ImageModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/image-modal/image-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-modal.component.html',
+  styleUrl: './image-modal.component.scss'
+})
+export class ImageModalComponent {
+
+}
