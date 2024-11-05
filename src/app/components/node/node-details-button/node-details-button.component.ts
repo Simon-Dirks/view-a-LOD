abe2a35c1f81ce49116f3fd9c507fee631ac1617
Simon <Simon-Dirks@users.noreply.github.com>
2024-11-05 14:06:10 +0100

    Add text to details button (closes #202)

--- src/app/components/node/node-details-button/node-details-button.component.ts
+++ src/app/components/node/node-details-button/node-details-button.component.ts
@@ -6,11 +6,12 @@ import { DetailsService } from '../../../services/details.service';
 import { NodeModel } from '../../../models/node.model';
 import { RouterLink } from '@angular/router';
 import { NodeService } from '../../../services/node.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-node-details-button',
   standalone: true,
-  imports: [NgIcon, NgIf, RouterLink],
+  imports: [NgIcon, NgIf, RouterLink, TranslatePipe],
   templateUrl: './node-details-button.component.html',
   styleUrl: './node-details-button.component.scss',
 })
