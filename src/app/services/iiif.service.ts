97393730107349020f14c6f5a541d219cdda5db3
Simon <mail@simondirks.com>
2025-02-19 10:30:01 +0100

    Use image URL in generated manifest

--- src/app/services/iiif.service.ts
+++ src/app/services/iiif.service.ts
@@ -6,49 +6,42 @@ import { Injectable } from '@angular/core';
 export class IIIFService {
   private _blobUrls: Set<string> = new Set();
 
-  private _mockManifest = {
-    '@context': 'http://iiif.io/api/presentation/3/context.json',
-    id: 'https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/manifest.json',
-    type: 'Manifest',
-    label: {
-      en: ['Image 1'],
-    },
-    items: [
-      {
-        id: 'https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/canvas/p1',
+  constructor() {}
+
+  generateManifest(imgUrls: string[]) {
+    return {
+      '@context': 'http://iiif.io/api/presentation/3/context.json',
+      id: 'https://example.org/manifest.json',
+      type: 'Manifest',
+      label: { en: ['Image Collection'] },
+      items: imgUrls.map((url, index) => ({
+        id: `https://example.org/canvas/p${index + 1}`,
         type: 'Canvas',
         height: 1800,
         width: 1200,
         items: [
           {
-            id: 'https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/page/p1/1',
+            id: `https://example.org/page/p${index + 1}/1`,
             type: 'AnnotationPage',
             items: [
               {
-                id: 'https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/annotation/p0001-image',
+                id: `https://example.org/annotation/p${index + 1}-image`,
                 type: 'Annotation',
                 motivation: 'painting',
                 body: {
-                  id: 'http://iiif.io/api/presentation/2.1/example/fixtures/resources/page1-full.png',
+                  id: url,
                   type: 'Image',
-                  format: 'image/png',
+                  format: 'image/jpeg',
                   height: 1800,
                   width: 1200,
                 },
-                target:
-                  'https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/canvas/p1',
+                target: `https://example.org/canvas/p${index + 1}`,
               },
             ],
           },
         ],
-      },
-    ],
-  };
-
-  constructor() {}
-
-  generateManifest(imgUrls: string[]) {
-    return this._mockManifest;
+      })),
+    };
   }
 
   createManifestBlob(imgUrls: string[]) {
